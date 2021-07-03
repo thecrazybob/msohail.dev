546bd0e3a003f84bf1a4da3788147750dc08bb42
Mohammed Sohail <itscrazybob@gmail.com>
2021-07-03 16:19:03 +0300

    wip

--- components/app-box.js
+++ components/app-box.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function AppBox(props) {
     return (
-        <div className="bg-white rounded-xl p-2 items-center filter shadow-md space-x-4">
+        <div className="bg-white dark:bg-gray-700 rounded-xl p-2 items-center filter shadow-md space-x-4">
             <div className="flex items-center space-x-4">
                 <div className=""><Image width={119} height={100} src={props.image}></Image></div>
                 <div class="flex flex-col space-y-2 justify-evenly">
