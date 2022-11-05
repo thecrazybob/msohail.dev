87151d164ed6d51a96f19c18023841b89a9b869d
Mohammed Sohail <hey@msohail.dev>
2022-11-05 23:59:20 +0300

    Add border bottom to title bar

--- components/title-bar.js
+++ components/title-bar.js
@@ -3,7 +3,7 @@ import arrowDown from "../public/arrow-down.svg";
 
 export default function TitleBar(props) {
   return (
-    <section className="mb-10 pb-20">
+    <section className="mb-10 border-b-2 border-b-black pb-16 dark:border-b-white">
       <div className="grid items-center gap-y-6 md:grid-cols-2 md:gap-0">
         <h2 className="text-5xl">{props.title}</h2>
         <div className="justify-self-end text-2xl text-gray-800 dark:text-gray-300">
