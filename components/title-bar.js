c37edac2d05d6e10d761c034f1946a27486930db
Mohammed Sohail <hey@msohail.dev>
2022-11-14 04:41:37 +0300

    Styling improvements to titlebar

--- components/title-bar.js
+++ components/title-bar.js
@@ -4,9 +4,9 @@ import arrowDown from "../public/arrow-down.svg";
 export default function TitleBar(props) {
   return (
     <section className="mb-10 border-b-2 border-b-black pb-16 dark:border-b-white">
-      <div className="grid items-center gap-y-6 md:grid-cols-2 md:gap-0">
+      <div className="flex flex-col gap-y-6">
         <h2 className="text-5xl">{props.title}</h2>
-        <div className="justify-self-end text-2xl text-gray-800 dark:text-gray-300">
+        <div className="text-2xl text-gray-800 dark:text-gray-300">
           {props.description}
         </div>
       </div>
